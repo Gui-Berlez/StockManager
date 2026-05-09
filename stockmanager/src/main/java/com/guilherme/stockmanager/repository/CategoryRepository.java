7c6cc67b18d012c9b7d958d969cdf745300536f1
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2026-05-09 14:42:53 -0300

    Classe CategoryRepository

--- stockmanager/src/main/java/com/guilherme/stockmanager/repository/CategoryRepository.java
+++ stockmanager/src/main/java/com/guilherme/stockmanager/repository/CategoryRepository.java
@@ -1,6 +1,9 @@
 package com.guilherme.stockmanager.repository;
 
-public class CategoryRepository {
+import com.guilherme.stockmanager.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 
 
