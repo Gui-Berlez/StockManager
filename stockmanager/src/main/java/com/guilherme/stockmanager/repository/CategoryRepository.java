3d46a71cf5b02ba87efcf35f0d2139e79c3b7e2d
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2026-05-12 23:07:57 -0300

    Classe CategoryRepository

--- stockmanager/src/main/java/com/guilherme/stockmanager/repository/CategoryRepository.java
+++ stockmanager/src/main/java/com/guilherme/stockmanager/repository/CategoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-
-
+    // O Spring gera o SQL automáticamente pelo nome do metodo!!!
+    // SELECT * FROM categories WHERE name = ?
+    boolean existsByName(String name);
 }
